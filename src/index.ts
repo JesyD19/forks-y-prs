bd6440c5eee297e1b1136dc3c9a125ebef2dc057
Jesica Dominguez <jesy.dom19@gmail.com>
2025-06-10 18:39:38 -0600

    se corrigio el metodo addProducts de la clase User y se agrego el metodo findProductsBelow(precioBase:number) de la clase Product

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(price: number): Product[] {
+    const findProducts = products.filter((pr) => pr.price < price);
+    return findProducts;
+  }
 }
 
 class User {
@@ -20,9 +25,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    // esto no funciona:
-    this.products.push(newProducts);
-    // pista: push no suma muchos items (agrega de a uno)
+    this.products = [...this.products, ...newProducts];
   }
 }
 
